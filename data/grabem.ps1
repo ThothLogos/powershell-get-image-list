880d61d6aff093cc3a8672f87d99896c26815819
ptol <naughton.joshua@gmail.com>
2020-04-12 19:58:17 -0400

    Attempt to find coverimages list automatically

--- data/grabem.ps1
+++ data/grabem.ps1
@@ -2,16 +2,37 @@
 $outdir = Get-Date -Format "HH.mm.ss-MM-dd-yyyy"
 $datadir = ".\data"
 $download_dir = "C:\Users\$env:UserName\Downloads"
+$coverlist_pattern = "*coverimages*"
 
-echo "User holds: $download_dir"
+# Find the browser's output file, first check the default system download dir
+if (Test-Path -Path "$download_dir") {
+  $newest = Get-ChildItem -Path "$download_dir" -File -Filter $coverlist_pattern |
+            Sort-Object LastAccessTime -Descending |
+            Select-Object -First 1 | % { $_.FullName }
+  if (!($newest)) {
+    echo "Not found in Downloads, trying local.." 
+  } else {
+    echo "Newest was found in Downloads: $newest"
+  }
+} else {
+  echo "`nDefault download folder ($download_dir) not found, attempting search."
+  echo "`n`t(This may take a minute! Please wait...)`n"
+  $newest = Get-ChildItem -Path C:\ -Filter $coverlist_pattern -Recurse -ErrorAction SilentlyContinue -Force |
+            Sort-Object LastAccessTime -Descending |
+            Select-Object -First 1 | % { $_.FullName }
+  echo "Search results: $newest"
+}
 
-# If this folder doesn't exist, make it so
+### At this point, hopefully we found the file...
+if (!($newest)) { exit } # If not, let's bail
+
+# If a timestamped folder doesn't exist, make it so - will hold the pics before zipping
 if (-Not (Test-Path -Path "$datadir\$outdir")) {
   New-Item -ItemType Directory -Force -Path $datadir\$outdir
 }
 
 # Loop over each line of the input file
-foreach($line in Get-Content .\examplelist.txt) {
+foreach($line in Get-Content $newest) {
   # We expect an image URL, so chop off the last bit to use as a filename
   $pic=$line.Split("{/}")[-1]
   # Equivalent of wget, snatch the image and save it as the name we just split off
@@ -28,3 +49,5 @@ Compress-Archive -Path $datadir\$outdir\* -Update -DestinationPath ".\Cover Imag
 
 # Remove the timestamped folder which was holding our raw images
 Remove-Item -Recurse -Force "$datadir\$outdir"
+
+sleep 10
